12c7a771d149359440a93fc5a937e32f378ba6df
Alex Rodriguez <djale2008@gmail.com>
2022-12-18 21:56:29 -0500

    test

--- src/test/java/com/api/test/TestRunner.java
+++ src/test/java/com/api/test/TestRunner.java
@@ -10,16 +10,16 @@ import io.cucumber.junit.CucumberOptions.SnippetType;
 @CucumberOptions(
 		plugin = {"pretty:target/cucumber/cucumber.txt",
 				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
-				//"html:target/cucumber/report",
+//				"html:target/cucumber/report",
 				"json:target/cucumber/cucumber.json",
-				"com.api.utils.MyTestListener"
+//				"com.api.utils.MyTestListener"
 		}
 		,features= {"src/test/resources/features"}
 		,glue = {"com.api.stepdefinition"}
 		//,dryRun = true
 		,monochrome = true
 		,snippets = SnippetType.CAMELCASE
-		,tags = "@bookerAPI"
+		,tags = "@createBookingDataTable"
 		//,publish = true
 		)
 public class TestRunner {
